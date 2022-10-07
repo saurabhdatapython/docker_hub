27c2e9c44e24e0e62c7f6c1ed75455ede02d085c
saurabhdata <saurabhdata.python@gmail.com>
2022-10-07 23:24:26 +0530

    hi

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
-#!/usr/bin/env python
-
 from wsgiref import simple_server
 from flask import Flask, request, render_template
 from flask import Response
-#from flask_cors import CORS, cross_origin
 import os
+from flask_cors import CORS, cross_origin
+from prediction_Validation_Insertion import pred_validation
+from trainingModel import trainModel
+from training_Validation_Insertion import train_validation
+import flask_monitoringdashboard as dashboard
+from predictFromModel import prediction
+import json
 
 
 
