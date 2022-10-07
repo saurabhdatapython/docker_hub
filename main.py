694162d3939b0c76ee917ba9e19b204748d3a4f1
saurabhdata <saurabhdata.python@gmail.com>
2022-10-07 18:57:01 +0530

    hi

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from wsgiref import simple_server
 from flask import Flask, request, render_template
 from flask import Response
+from best_model_finder.tuner import Model_Finder
 import os
 from flask_cors import CORS, cross_origin
 
@@ -9,6 +10,13 @@ app = Flask(__name__)
 
 @app.route("/", methods=['GET', 'POST'])
 def index():
+    file = open("sav.txt",'a+')
+    message = "hi "
+    obj  = Model_Finder(file,message)
+    x = "path"
+    y = "sath"
+    obj.get_best_params_for_random_forest(x,y)
+
     return "flask app is running change"
 
 port = int(os.getenv("PORT", 5000))
