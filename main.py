0e5d14b947c1112c4fa27fa957c3fede7312779f
saurabhdata <saurabhdata.python@gmail.com>
2022-10-07 19:05:15 +0530

    hi

--- main.py
+++ main.py
@@ -10,14 +10,7 @@ app = Flask(__name__)
 
 @app.route("/", methods=['GET', 'POST'])
 def index():
-    file = open("sav.txt",'a+')
-    message = "hi "
-    obj  = Model_Finder(file,message)
-    x = "path"
-    y = "sath"
-    obj.get_best_params_for_random_forest(x,y)
-
-    return "flask app is running change"
+   return "flask app is running change"
 
 port = int(os.getenv("PORT", 5000))
 if __name__ == "__main__":
